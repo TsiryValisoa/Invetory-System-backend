22b1b7f9cda3a6094bfe389666dda35e76fcb4ef
Tsiry Valisoa <rotsiniaina.tsiry@gmail.com>
2025-11-17 15:30:43 +0300

    refact: optimize import

--- src/main/java/mg/tsiry/invetory_management_system/dto/UserDto.java
+++ src/main/java/mg/tsiry/invetory_management_system/dto/UserDto.java
@@ -2,11 +2,9 @@ package mg.tsiry.invetory_management_system.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
-import mg.tsiry.invetory_management_system.data.entities.Transaction;
 import mg.tsiry.invetory_management_system.enums.UserRole;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
 public class UserDto {

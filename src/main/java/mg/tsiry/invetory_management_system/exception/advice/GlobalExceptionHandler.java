a481f19ff859798cee3d14fd854c861f6afc7245
Tsiry Valisoa <rotsiniaina.tsiry@gmail.com>
2025-11-17 15:29:47 +0300

    feat: add handler for validation

--- src/main/java/mg/tsiry/invetory_management_system/exception/advice/GlobalExceptionHandler.java
+++ src/main/java/mg/tsiry/invetory_management_system/exception/advice/GlobalExceptionHandler.java
@@ -6,8 +6,13 @@ import mg.tsiry.invetory_management_system.exception.NameValueRequiredException;
 import mg.tsiry.invetory_management_system.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -54,5 +59,18 @@ public class GlobalExceptionHandler {
                 .build();
 
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = "message";
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
